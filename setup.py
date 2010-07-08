61ef21357682ec423434dfce17fcf1e69cc3a5d4
Rob <rob@merlin.(none)>
2010-07-08 15:52:46 -0700

    fix readme

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     name='NoseJS',
     version=version,
     description="A Nose plugin for integrating JavaScript tests into a Python test suite.",
-    long_description=open("README.txt",'r').read(),
+    long_description=open("README",'r').read(),
     zip_safe=False,
     packages=find_packages(),
     include_package_data=True,
