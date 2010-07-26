d5e033fc636fc9be5fa677823eedac2429a7241a
Rob <rob@milo.com>
2010-07-25 17:14:43 -0700

    file not necessary

--- selftest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# fixme: this is the quick and dirty way I have been testing
-# the plugin.  needs self-contained unit tests
-source _env/bin/activate
-cd ~/dev/fudge/
-nosetests -v --with-javascript --rhino-jar  ~/src/rhino1_7R1/js.jar --with-dom --javascript-dir javascript/fudge/tests/ $@
